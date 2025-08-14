278472602084dcf50b8cf8acb8f0aad97c6f426b
YongYingXie <1191456925@qq.com>
2025-08-14 09:42:40 +0800

    feat: add basic code rule

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next"],
+    ignores: [".next", "node_modules", "dist", "build", "coverage"],
   },
   ...compat.extends("next/core-web-vitals"),
   {
@@ -18,6 +18,7 @@ export default tseslint.config(
       ...tseslint.configs.stylisticTypeChecked,
     ],
     rules: {
+      // TypeScript 相关规则
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
@@ -33,6 +34,40 @@ export default tseslint.config(
         "error",
         { checksVoidReturn: { attributes: false } },
       ],
+      
+      // 代码风格规则
+      "indent": ["error", 2, { "SwitchCase": 1 }],
+      "quotes": ["error", "double", { "avoidEscape": true }],
+      "semi": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "space-before-function-paren": ["error", {
+        "anonymous": "always",
+        "named": "never",
+        "asyncArrow": "always"
+      }],
+      "space-before-blocks": "error",
+      "keyword-spacing": "error",
+      "space-infix-ops": "error",
+      "eol-last": "error",
+      "no-trailing-spaces": "error",
+      "no-multiple-empty-lines": ["error", { "max": 2 }],
+      "no-console": "warn",
+      "no-debugger": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      
+      // React 相关规则
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/display-name": "off",
+      "react/no-unescaped-entities": "off",
+      
+      // Next.js 相关规则
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off",
     },
   },
   {
@@ -42,6 +77,11 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         projectService: true,
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
   },
